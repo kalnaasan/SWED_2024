93054f5e74bf81ec907e66397b3df26871ea5f90
Kaddour Alnaasan <kaddour@alnaasan>
2024-05-03 14:48:10 +0200

    fix name

--- Exercise_1/src/main/java/edu/fra/uas/App.java
+++ Exercise_1/src/main/java/edu/fra/uas/App.java
@@ -12,7 +12,7 @@ public class App {
         int numberWithMaxSteps = 1;
 
         for (int i = 1; i < limit; i++) {
-            int steps = findStepsToReachOne(i);
+            int steps = ulamFunc(i);
             System.out.println("Find step " + i + " with max steps " + steps);
             if (steps > maxSteps) {
                 maxSteps = steps;
@@ -25,13 +25,13 @@ public class App {
 
     }
 
-    private static int findStepsToReachOne(long n) {
+    private static int ulamFunc(long n) {
         if (n == 1) return 0;
 
         if (!memory.isEmpty() && memory.containsKey(n)) return memory.get(n);
 
         long next = n % 2 == 0 ? n / 2 : 3 * n + 1;
-        int steps = 1 + findStepsToReachOne(next);
+        int steps = 1 + ulamFunc(next);
 
         memory.put(n, steps);
         return steps;
