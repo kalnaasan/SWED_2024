67bf3bddb29b4a3c9fb6fd98aade52ea1d68397b
Kaddour Alnaasan <kaddour@alnaasan>
2024-05-31 09:46:50 +0200

    create users- and subscriptions-route

--- WebsiteMonitor/Frontend/src/app/app-routing.module.ts
+++ WebsiteMonitor/Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SubscriptionComponent} from "./component/subscription/subscription.component";
+import {UserComponent} from "./component/user/user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'subscriptions', component: SubscriptionComponent, pathMatch: 'full'},
+  {path: 'users', component: UserComponent, pathMatch: 'full'},
+  {path: '', redirectTo: '/subscriptions', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
