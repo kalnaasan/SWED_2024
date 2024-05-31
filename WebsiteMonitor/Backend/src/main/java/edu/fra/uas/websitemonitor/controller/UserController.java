834088008a943deea8e1d595d7dcf310d027e5fd
Kaddour Alnaasan <kaddour@alnaasan>
2024-05-31 09:41:02 +0200

    add cross-origin

--- WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/controller/UserController.java
+++ WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/v1/users")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     // User service to handle business logic
