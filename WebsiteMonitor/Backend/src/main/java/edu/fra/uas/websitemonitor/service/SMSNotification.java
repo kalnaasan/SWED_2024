397291d11e2e01820b863e815853d4153ed740d5
Kaddour Alnaasan <kaddour@alnaasan>
2024-05-31 09:39:43 +0200

    add logger

--- WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/service/SMSNotification.java
+++ WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/service/SMSNotification.java
@@ -1,16 +1,19 @@
 package edu.fra.uas.websitemonitor.service;
 
 import edu.fra.uas.websitemonitor.model.Subscription;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Qualifier("SMSNotification")
+@Slf4j
 public class SMSNotification implements Notification {
 
     @Override
     public void sendNotification(Subscription subscription) {
+      log.info("SMS sending to {}", subscription.getUser().getPhone());
     }
 
 }
