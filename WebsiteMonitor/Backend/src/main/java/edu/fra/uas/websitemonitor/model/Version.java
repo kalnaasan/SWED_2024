4ba9c4a0c38d8f0fc35fee66bbcd099e7a93ece5
Kaddour Alnaasan <kaddour@alnaasan>
2024-06-14 11:48:54 +0200

    implement Observable in version

--- WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/model/Version.java
+++ WebsiteMonitor/Backend/src/main/java/edu/fra/uas/websitemonitor/model/Version.java
@@ -1,14 +1,16 @@
 package edu.fra.uas.websitemonitor.model;
 
+import edu.fra.uas.websitemonitor.observer.Observable;
+import edu.fra.uas.websitemonitor.observer.Observer;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +18,10 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Builder
@@ -26,7 +31,7 @@ import java.time.LocalDateTime;
 @ToString
 @Entity
 @Table(name = "versions")
-public class Version {
+public class Version implements Observable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,6 +39,26 @@ public class Version {
     private String content;
     private LocalDateTime createdAt;
     @ManyToOne
-    @JoinColumn(name = "subscriptionId", nullable = false)
+    @JoinColumn(name = "subscription_Id", nullable = false)
     private Subscription subscription;
+    @Transient
+    private Set<Observer> observers = new HashSet<>();
+
+    public Version(String content, LocalDateTime now, Subscription subscription) {
+        this.content = content;
+        this.createdAt = now;
+        this.subscription = subscription;
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        this.observers.forEach(observer -> observer.update(this));
+    }
 }
